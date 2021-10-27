ab1f0510202fed84263a939118e16a98fed23468
Ramon Ayco Jr <ramon.aycojr@infor.com>
2021-10-27 17:06:12 +0800

    feat: add state winner

--- src/index.js
+++ src/index.js
@@ -53,15 +53,16 @@ class Game extends React.Component {
           squares: Array(9).fill(null),
         },
       ],
+      stepNumber: 0,
       isXNext: true,
       winner: null,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
-    const winner = calcluateWinner(current.squares);
+    const winner = calculateWinner(current.squares);
     const squares = current.squares.slice();
     if (isNaN(i) || !!winner || !!squares[i]) {
       return;
@@ -73,19 +74,39 @@ class Game extends React.Component {
           squares,
         },
       ]),
+      stepNumber: history.length,
       isXNext: !this.state.isXNext,
+      winner,
     });
+  }
+
+  jumpTo(i) {
+    if (!this.state.winner) {
+      console.log(i);
+      this.setState({
+        stepNumber: i,
+        isXNext: i % 2 === 0,
+      });
+    }
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
-    const winner = calcluateWinner(current.squares);
+    const current = history[this.state.stepNumber];
+    const winner = this.state.winner;
     const label = !!winner ? "Winner: " : "Next player: ";
     const value = winner || this.state.isXNext ? "X" : "O";
     const status = label + value;
 
     console.log(history);
+    const moves = history.map((step, index) => {
+      const desc = index ? "Go to move #" + index : "Go to game start";
+      return (
+        <li key={index}>
+          <button onClick={() => this.jumpTo(index)}>{desc}</button>
+        </li>
+      );
+    });
 
     return (
       <div className="game">
@@ -97,7 +118,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
@@ -108,7 +129,7 @@ class Game extends React.Component {
 
 ReactDOM.render(<Game />, document.getElementById("root"));
 
-function calcluateWinner(squares) {
+function calculateWinner(squares) {
   let winner = null;
   const lines = [
     [0, 1, 2],
@@ -119,6 +140,7 @@ function calcluateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+
   lines.forEach(([a, b, c]) => {
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       winner = squares[a];
